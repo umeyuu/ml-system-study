4f8942a1e09689572fa2496b6e3648491bbf1de7
umeyuu <91179464+umeyuu@users.noreply.github.com>
2023-01-27 01:53:19 +0900

    api conect postgresSQL

--- model_db/src/api/app.py
+++ model_db/src/api/app.py
@@ -3,9 +3,12 @@ from logging import getLogger
 from fastapi import FastAPI
 from src.api.routers import api, health
 from src.configurations import APIConfigurations
+from src.db.database import engine
+from src.db.initialize import initialize_table
 
 logger = getLogger(__name__)
 
+initialize_table(engine=engine, checkfirst=True)
 
 app = FastAPI(
     title=APIConfigurations.title,
